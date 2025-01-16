c655e397594afe029ce6500b3ce75d7e03fabe5c
Bibek <137331749+immbibek@users.noreply.github.com>
2025-01-16 22:29:54 +0530

    Hero section created

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
 
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="mt-40">
-      <Button>Click me</Button>
+       <Hero/>
     </div>
   );
 }
